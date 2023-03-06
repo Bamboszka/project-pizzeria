db246c4ed83ae1b1ea95831a248769868fec6499
Paulina Ruta <paulina.ruta@outlook.com>
2023-03-06 15:39:08 +0100

    Add init accordion - update

--- src/js/script.js
+++ src/js/script.js
@@ -87,6 +87,7 @@
         event.preventDefault();
         /* find active product (product that has active class) */
         const activeProduct = document.querySelector(select.all.menuProductsActive);
+        console.log(activeProduct);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
         if (activeProduct !== null && activeProduct !== thisProduct.element) {
           activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
